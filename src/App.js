3803134a4629a3af5d72341348a5631e11c56d60
Mateusz Walter <wuoelte@gmail.com>
2021-07-30 19:52:55 +0200

    task2

--- src/App.js
+++ src/App.js
@@ -10,35 +10,15 @@ const styles= {
   }
 };
 
-const posts = [
-  { id: 1, title: 'Pilne: Co to był za dzień!', intro: 'Tego świat jeszcze nie widział'},
-  { id: 2, title: 'Wszyscy zazdroszą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie wymyślił'},
-  { id: 3, title: 'Adam Małysz Zgolił wąs',
-    intro: 'Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy'
-  }
-];
+  const { v4: uuidv4 } = require('uuid');
+  uuidv4();
 
-
-function News({ id, title, intro}) {
-  return(
-    <div style={styles}>
-      <h1>{id}</h1>
-      <h2>{title}</h2>
-      <p style={styles.para}>{intro.slice(0,25)+`...`}</p>
-    </div>
-  );
-}
-
-function BlogTile({ id, title, intro }) {
-   return(
-        posts.map(( elem, index ) => (
-        <News key={`news-${index}`} id={elem.id} title={elem.title} intro={elem.intro} />
-    )));};
 
 function App() {
+  const { v4: uuidv4 } = require('uuid');
   return (
     <div>
-        <BlogTile />
+        {uuidv4()}
     </div>
   );
 }
